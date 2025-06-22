ea875eca9aebf35b7e5aa0fef67aa743e12ce13a
nhankotu <nhankotu@gmail.com>
2025-06-22 15:25:49 +0700

    Update emailConfig.js

--- backend/config/emailConfig.js
+++ backend/config/emailConfig.js
@@ -10,8 +10,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
 
-    // user: "tranduchai3241@gmail.com", // Thay đổi thành email của bạn
-    // pass: "Nhan5404@", // Thay đổi thành mật khẩu ứng dụng của bạn
+
   },
 });
 
